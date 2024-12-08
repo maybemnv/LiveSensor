3da3ab50559ff617eb6b9da4831d66a177aeb6c9
maybemnv <manavkauahal99@gmail.com>
2024-12-08 18:12:02 +0530

    Initial  commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+from typing import List
+setup(
+    name="aps",
+    version="0.1", 
+    author="Manav",
+    author_email="manavkauahal99@gmail.com",
+    description="A predictive maintenance system for monitoring air pressure systems.",
+    url="https://github.com/maybemnv/LiveSensor",
+    packages=find_packages(),
+)
