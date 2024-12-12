672d565e4357b0866bbade03c817c592655944ce
maybemnv <manavkauahal99@gmail.com>
2024-12-12 15:06:31 +0530

    Next commit

---
+++ sensor/exception.py
@@ -0,0 +1,20 @@
+import sys 
+import os
+
+def error_message_detail(error,error_detial:sys):
+    _,_exc_tb=sys.error_detial.exc_info()
+    filename=exc_tb.tb_frame.f_code.co_filename
+    
+    error_message="error accoured and the file name is [{0}] and the line number is [{1}] and error is [{2}]".format(filename,exc_tb.tb_lineno,str(error))
+    
+    return error_message
+class SensorException(Exception):
+    def __init__(self,error_message,error_details:sys):
+    
+         super().__init__(self,error_message)
+    
+         self.error_message=error_message_detail(error_message,error_details)
+         
+    def __str__(self):
+        return self.error_message
+    
---
+++ sensor/logger.py
@@ -0,0 +1,19 @@
+import logging
+import os
+from datetime import datetime
+
+# Setting up the log file name with the current timestamp
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+# Creating a directory to store logs
+logs_path = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_path, exist_ok=True)
+
+# Defining the full log file path
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+# Configuring the logging settings
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO  
